4c19438c462b35287e99896cda3d9b5301702b46
Elusive <elusivecodes@gmail.com>
2022-01-09 18:14:30 +1000

    Updated tests

--- tests/UserAgentTest.php
+++ tests/UserAgentTest.php
@@ -12,7 +12,7 @@ final class UserAgentTest extends TestCase
 
     public function testGetAgentStringDesktop(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36',
             (new UserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'))->getAgentString()
         );
@@ -20,7 +20,7 @@ final class UserAgentTest extends TestCase
 
     public function testGetAgentStringMobile(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
             (new UserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'))->getAgentString()
         );
@@ -28,7 +28,7 @@ final class UserAgentTest extends TestCase
 
     public function testGetAgentStringRobot(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
             (new UserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'))->getAgentString()
         );
@@ -36,7 +36,7 @@ final class UserAgentTest extends TestCase
 
     public function testGetBrowserDesktop(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             'Chrome',
             (new UserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'))->getBrowser()
         );
@@ -44,7 +44,7 @@ final class UserAgentTest extends TestCase
 
     public function testGetBrowserMobile(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             'Safari',
             (new UserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'))->getBrowser()
         );
@@ -52,23 +52,21 @@ final class UserAgentTest extends TestCase
 
     public function testGetBrowserRobot(): void
     {
-        $this->assertEquals(
-            null,
+        $this->assertNull(
             (new UserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'))->getBrowser()
         );
     }
 
     public function testGetMobileDesktop(): void
     {
-        $this->assertEquals(
-            null,
+        $this->assertNull(
             (new UserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'))->getMobile()
         );
     }
 
     public function testGetMobileMobile(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             'Apple iPhone',
             (new UserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'))->getMobile()
         );
@@ -76,15 +74,14 @@ final class UserAgentTest extends TestCase
 
     public function testGetMobileRobot(): void
     {
-        $this->assertEquals(
-            null,
+        $this->assertNull(
             (new UserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'))->getMobile()
         );
     }
 
     public function testGetPlatformDesktop(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             'Windows 7',
             (new UserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'))->getPlatform()
         );
@@ -92,7 +89,7 @@ final class UserAgentTest extends TestCase
 
     public function testGetPlatformMobile(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             'iOS',
             (new UserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'))->getPlatform()
         );
@@ -100,7 +97,7 @@ final class UserAgentTest extends TestCase
 
     public function testGetPlatformRobot(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             'Unknown Platform',
             (new UserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'))->getPlatform()
         );
@@ -108,23 +105,21 @@ final class UserAgentTest extends TestCase
 
     public function testGetRobotDesktop(): void
     {
-        $this->assertEquals(
-            null,
+        $this->assertNull(
             (new UserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'))->getRobot()
         );
     }
 
     public function testGetRobotMobile(): void
     {
-        $this->assertEquals(
-            null,
+        $this->assertNull(
             (new UserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'))->getRobot()
         );
     }
 
     public function testGetRobotRobot(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             'Googlebot',
             (new UserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'))->getRobot()
         );
@@ -132,7 +127,7 @@ final class UserAgentTest extends TestCase
 
     public function testGetVersionDesktop(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             '47.0.2526.111',
             (new UserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'))->getVersion()
         );
@@ -140,7 +135,7 @@ final class UserAgentTest extends TestCase
 
     public function testGetVersionMobile(): void
     {
-        $this->assertEquals(
+        $this->assertSame(
             '604.1',
             (new UserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'))->getVersion()
         );
@@ -148,80 +143,70 @@ final class UserAgentTest extends TestCase
 
     public function testGetVersionRobot(): void
     {
-        $this->assertEquals(
-            null,
+        $this->assertNull(
             (new UserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'))->getVersion()
         );
     }
 
     public function testIsBrowserDesktop(): void
     {
-        $this->assertEquals(
-            true,
+        $this->assertTrue(
             (new UserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'))->isBrowser()
         );
     }
 
     public function testIsBrowserMobile(): void
     {
-        $this->assertEquals(
-            true,
+        $this->assertTrue(
             (new UserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'))->isBrowser()
         );
     }
 
     public function testIsBrowserRobot(): void
     {
-        $this->assertEquals(
-            false,
+        $this->assertFalse(
             (new UserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'))->isBrowser()
         );
     }
 
     public function testIsMobileDesktop(): void
     {
-        $this->assertEquals(
-            false,
+        $this->assertFalse(
             (new UserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'))->isMobile()
         );
     }
 
     public function testIsMobileMobile(): void
     {
-        $this->assertEquals(
-            true,
+        $this->assertTrue(
             (new UserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'))->isMobile()
         );
     }
 
     public function testIsMobileRobot(): void
     {
-        $this->assertEquals(
-            false,
+        $this->assertFalse(
             (new UserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'))->isMobile()
         );
     }
 
     public function testIsRobotDesktop(): void
     {
-        $this->assertEquals(
-            false,
+        $this->assertFalse(
             (new UserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36)'))->isRobot()
         );
     }
 
     public function testIsRobotMobile(): void
     {
-        $this->assertEquals(
-            false,
+        $this->assertFalse(
             (new UserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'))->isRobot()
         );
     }
 
     public function testIsRobotRobot(): void
     {
-        $this->assertEquals(
-            true,
+        $this->assertTrue(
             (new UserAgent('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'))->isRobot()
         );
     }
@@ -230,32 +215,31 @@ final class UserAgentTest extends TestCase
     {
         $userAgent = new UserAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36');
 
-        $this->assertEquals(
+        $this->assertSame(
             $userAgent,
             $userAgent->setAgentString('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36)')
         );
 
-        $this->assertEquals(
+        $this->assertSame(
             'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36)',
             $userAgent->getAgentString()
         );
 
-        $this->assertEquals(
+        $this->assertSame(
             'Chrome',
             $userAgent->getBrowser()
         );
 
-        $this->assertEquals(
-            null,
+        $this->assertNull(
             $userAgent->getMobile()
         );
 
-        $this->assertEquals(
+        $this->assertSame(
             'Windows 7',
             $userAgent->getPlatform()
         );
 
-        $this->assertEquals(
+        $this->assertSame(
             '47.0.2526.111',
             $userAgent->getVersion()
         );
